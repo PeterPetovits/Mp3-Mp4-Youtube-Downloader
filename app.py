f4facbd2457745aeb496fad30bf7b1cfcbf0f467
Peter <peterp@outlook.com.gr>
2022-11-26 20:20:49 +0200

    trimmer function complete and changes to the sequence of how things show up

--- app.py
+++ app.py
@@ -9,23 +9,29 @@ app = Flask(__name__)
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+#original app route to index page
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
+#template page after pressing mp3 option button 
 @app.route('/mp3_option_button', methods = ["GET", "POST"])
 def mp3_option_button():
     if request.method == "POST":
         return render_template('index.html', option_form_mp3 = "mp3")
     return render_template('index.html')
-        
 
+      
+#template page after pressing mp4 option button
 @app.route('/mp4_option_button', methods = ["GET", "POST"])
 def mp4_option_button():
     if request.method == "POST":
         return render_template('index.html', option_form_mp4 = "mp4")
     return render_template('index.html')
 
+
+#mp3 download function after link inserted
 @app.route('/mp3_download', methods= ["GET", "POST"])
 def mp3_download():
     if request.method == "POST":
@@ -45,11 +51,21 @@ def mp3_download():
         base, ext = os.path.splitext(out_file)
         new_file = base + '.mp3'
         os.rename(out_file, new_file)
+
         global fileNameDownloadPath
         fileNameDownloadPath = new_file
-        return render_template('index.html', result = "Download Complete", file = fileNameDownloadPath, option_form_mp3 = "mp3", metadata_editor_open = "open")
+
+        global songFileNameToTrimmer
+        useless, songFileNameToTrimmer = fileNameDownloadPath.split('/./')
+        os.rename(songFileNameToTrimmer, "temp-download.mp3")
+        os.system("ffmpeg -i temp-download.mp3 -ab 320k temp.mp3")
+        os.rename("temp.mp3", songFileNameToTrimmer)
+        os.remove("temp-download.mp3")
+        return render_template('index.html', result = "Download Complete", file = fileNameDownloadPath, option_form_mp3 = "mp3", trimmer_open = "open")
     return render_template('index.html')
 
+
+#mp4 download function after link inserted
 @app.route('/mp4_download', methods= ["GET", "POST"])
 def mp4_download():
     if request.method == "POST":
@@ -86,7 +102,24 @@ def mp4_download():
         return render_template('index.html', result = "Download Complete", filePath = videoFileName, option_form_mp4 = "mp4")
     return render_template('index.html')
 
+#mp3 trimmer template after mp3 file download complete
+@app.route('/trimmer_editor', methods = ["GET", "POST"])
+def trimmer_editor():
+    if request.method == "POST":
 
+        start = str(request.form.get("start-time"))
+        end = str(request.form.get("end-time"))
+
+        os.system("trimmer " + '"' + songFileNameToTrimmer + '"' + " --trim-start " + start + " --trim-end " + end + " --title tempTitle --artist tempArtist")
+
+        global fileNameDownloadPath
+        fileNameDownloadPath = str("tempArtist - tempTitle.mp3")
+
+        return render_template('index.html', result = "Download Complete", option_form_mp3 = "mp3", trimmer_open = "open", result_trimmer = "Trim Done", metadata_editor_open = "open")
+    return render_template('index.html')
+
+
+#mp3 metadata editor after mp3 file trimming complete
 @app.route('/mp3_metadata_editor', methods= ["GET", "POST"])
 def mp3_metadata_editor():
     if request.method == "POST":
@@ -109,7 +142,11 @@ def mp3_metadata_editor():
         if cover_art_file :
             cover_art_file.save(os.path.join(app.config['UPLOAD_FOLDER'], cover_art_file.filename))
 
-        useless, songFileName = fileNameDownloadPath.split('/./')
+        #useless, songFileName = fileNameDownloadPath.split('/./')
+
+        print(fileNameDownloadPath)
+        songFileName = fileNameDownloadPath
+        print(songFileName)
 
         os.rename(songFileName, "temp.mp3")
         os.system("ffmpeg -i temp.mp3 -ab 320k temp2.mp3")
@@ -141,11 +178,14 @@ def mp3_metadata_editor():
 
                 os.rename(songFileName, artist + " - " + title + ".mp3")
                 os.remove(UPLOAD_FOLDER + "/" + cover_art_file.filename)
+                global finalFileNameMp3
+                finalFileNameMp3 = artist + " - " + title + ".mp3"
         except IOError:
             IOError
         
-        return render_template('index.html', result = "Download Complete", file = fileNameDownloadPath, option_form_mp3 = "mp3")
+        return render_template('index.html', result = "Download Complete", option_form_mp3 = "mp3", metadata_editor_open = "open", trimmer_open = "open")
     return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
